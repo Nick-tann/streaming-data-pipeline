579b7212c85306eb3ffd9545b5d716b2052cf3be
Nicholas <nictann96@gmail.com>
2024-09-29 21:36:27 +0800

    feat: Updated utils spotify manager class to include methods for retrieving user profile and user playlist

--- scripts/retrieve_spotify_data.py
+++ scripts/retrieve_spotify_data.py
@@ -1,19 +1,19 @@
 import json
+import base64
 import requests
+import logging
 from utils import utils
 from requests.auth import HTTPBasicAuth
 from dotenv import dotenv_values
 
 #Set up config and credentials
-scope = "user-library-read, user-top-read"
+logger = logging.getLogger(__name__)
+# scope = "user-library-read, user-top-read"
+utils.set_logger()
 
 url_file_name = "config/urls.json"
 with open(url_file_name) as f:
     urls = json.load(f)
-
-# BASE_URL = 'https://api.spotify.com/v1'
-# AUTH_URL = 'https://accounts.spotify.com/authorize'
-# OAUTH_TOKEN_URL = 'https://accounts.spotify.com/api/token'
 
 BASE_URL = urls['BASE_URL']
 AUTH_URL = urls['AUTH_URL']
@@ -23,40 +23,27 @@ REDIRECT_URL = urls['REDIRECT_URL']
 #Get credentials from env
 client_id = dotenv_values("credentials/spotify-cred.env")['client-id']
 client_secret = dotenv_values("credentials/spotify-cred.env")['client-secret']
-# redirect_url = "http://localhost:8080/callback"
 
-#Prepare header and payload to get access token
-token_headers = {
-    "Content-Type": "application/x-www-form-urlencoded"
-}
+user_id = "test"
 
-token_payload = {
-    'grant_type': 'client_credentials',
-    'client_id': client_id,
-    'client_secret': client_secret,
-}
+# #The Weeknd
+# artist_id = "1Xyo4u8uXC1ZmMpatF05PJ"
 
-#Post request to get Access token
-response = requests.post(
-    url = OAUTH_TOKEN_URL,
-    headers= token_headers,
-    data=token_payload
-)
+#Spotify manager to perform API calls
+spotify_manager = utils.SpotifyManager(client_id,client_secret,urls)
+with spotify_manager as sm:
+    # Get user profile
+    sm.user_profile(user_id)
 
-token = response.json()['access_token']
-
-#The Weeknd
-artist_id = "1Xyo4u8uXC1ZmMpatF05PJ"
-artist_url = BASE_URL + "/artists/" + artist_id
-
-#Prepare header to get artist
-headers = {
-    "Authorization":f"Bearer {token}"
-}
-
-response = requests.get(
-    url=artist_url,
-    headers=headers
-)
-
-print(json.dumps(response.json()))
+    # Get user public playlists
+    playlist_list = sm.user_public_playlist(user_id)
+    
+    #Test playlist
+    test_playlist = playlist_list[1]
+    playlist_id = test_playlist['id']
+    playlist_items_url = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
+    response = requests.get(
+        url = playlist_items_url,
+        headers= sm.headers
+    )
+    print(response.json()['items'][0])
--- scripts/utils/utils.py
+++ scripts/utils/utils.py
@@ -1,3 +1,4 @@
+import sys
 import logging
 import requests
 from dataclasses import dataclass
@@ -5,9 +6,89 @@ from functools import cached_property
 
 logger = logging.getLogger(__name__)
 
+#Setup logger
+def set_logger() -> None:
+    root_logger = logging.getLogger()
+    root_logger.setLevel(logging.INFO)
+
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.INFO)
+    formatter = logging.Formatter('[%(levelname)s] %(name)s - %(asctime)s : %(message)s',datefmt = "%Y-%m-%dT%H:%M:%S%z")
+    handler.setFormatter(formatter)
+    root_logger.addHandler(handler)
+    return
+
+
 @dataclass
 class SpotifyManager:
     client_id: str
     client_secret: str
-    urls:dict
+    urls: dict
 
+    @cached_property
+    def _token_headers(self)->dict:
+        return {
+        "Content-Type": "application/x-www-form-urlencoded"
+        }
+
+    @cached_property
+    def _token_payload(self)->dict:
+        return {
+        'grant_type': 'client_credentials',
+        'client_id': self.client_id,
+        'client_secret': self.client_secret
+        }
+    
+    def __enter__(self):
+        '''
+        When we initiate a Spotify Manager client, retrieve an access token.
+        
+        '''
+        logger.info("Instantiated a Spotify Manager Client.")
+        logger.info("Retrieving access token")
+        try:
+            response = requests.post(
+                url = self.urls['OAUTH_TOKEN_URL'],
+                headers= self._token_headers,
+                data=self._token_payload
+            )
+        except:
+            logger.error("Post request failed.")
+
+        if response.status_code != 200:
+            logger.error(f"Failed to retrieve token with error code: {response.status_code}")
+        else:
+            token = response.json()['access_token']
+            self.headers = {
+                "Authorization":f"Bearer {token}"
+            }
+            logger.info("Retrieved token successfully.")
+            return self
+
+    def __exit__(self, type, value, traceback):
+        logger.info("Closing Spotify Manager Client.")
+        
+
+    def user_profile(self, user_id:str)->dict[str, any]:
+        user_url = f'https://api.spotify.com/v1/users/{user_id}'
+        response = requests.get(
+            url = user_url,
+            headers= self.headers
+        )
+        user_json = response.json()
+        user_profile = {
+            "display_name":f"{user_json['display_name']}",
+            "spotify_url":f"{user_json['external_urls']['spotify']}",
+            "follower_count":f"{user_json['followers']['total']}"
+        }
+        logger.info(f"User {user_profile['display_name']} has {user_profile['follower_count']} number of followers.")
+        return
+
+    def user_public_playlist(self, user_id:str)->list:
+        playlist_url = f'https://api.spotify.com/v1/users/{user_id}/playlists'
+        response = requests.get(
+        url = playlist_url,
+        headers= self.headers
+        )
+        playlist_json = response.json()["items"]
+        return playlist_json
